ea70ef654c31c095e09674479ebf1b1274cecf01
Brandon Tobin <brandontobin@cox.net>
2016-11-21 12:40:49 -0700

    Have the login screen working

--- Android/Tuber/app/src/main/java/com/tuber/tuber/LoginActivity.java
+++ Android/Tuber/app/src/main/java/com/tuber/tuber/LoginActivity.java
@@ -1,13 +1,41 @@
 package com.tuber.tuber;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
+
+    // Dummy Data!!!!
+    String dummyLogin = "admin";
+    String dummyPassword = "admin";
+
+    Button loginButton;
+    EditText username, password;
+
+    TextView tx1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        username = (EditText) findViewById(R.id.loginUsername);
+        password = (EditText) findViewById(R.id.loginPassword);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(username.getText().toString().equals(dummyLogin) && password.getText().toString().equals(dummyPassword))
+                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
