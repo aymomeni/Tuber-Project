3a9e8dcbd128000a8b746b386a9bd11748cfab6c
Fahad Alothaimeen <uni_uk@hotmail.com>
2016-12-01 15:28:42 -0700

    login functionality completed

--- Android/Tuber/app/src/main/java/cs4000/tuber/LoginActivity.java
+++ Android/Tuber/app/src/main/java/cs4000/tuber/LoginActivity.java
@@ -11,11 +11,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.GetCallback;
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends Activity {
 
@@ -31,47 +33,68 @@ public class LoginActivity extends Activity {
 	login_button = (Button)findViewById(R.id.login_button);
 	register_button = (Button) findViewById(R.id.register_button);
 	username = (EditText) findViewById(R.id.login_username);
-	password = (EditText) findViewById(R.id.login_username);
+	password = (EditText) findViewById(R.id.login_password);
 
-	login_button.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		Log.i("Login Activity: ", "Login Clicked");
-		// check if the database contains thaye name and the correct password
-
-		Toast.makeText(getApplicationContext(),
-				"Redirecting...", Toast.LENGTH_SHORT).show();
-
-		ParseQuery<ParseUser> query = ParseQuery.getUserQuery(); //getQuery("User");
-
-		query.getInBackground("2uEYmgq7QP", new GetCallback<ParseUser>() {
-
+	  register_button.setOnClickListener(new View.OnClickListener() {
 		  @Override
-		  public void done(ParseUser user, ParseException e) {
-			Log.i("Login Activity: ", "Login Clicked2");
+		  public void onClick(View view) {
+			  String usernameStr = username.getText().toString();
+			  String passwordStr = password.getText().toString();
 
-			if (e == null && user != null) {
-			  Log.i("Login Activity: ", "Login Clicked3");
-			  String dbEmail = user.getString("username");
-			  String dbPW = user.getString("email");
+			  if(usernameStr.matches("") || passwordStr.matches("")){
+				  Toast.makeText(LoginActivity.this, "A username and password are required", Toast.LENGTH_SHORT).show();
+			  }else {
+				  ParseUser user = new ParseUser();
 
-			  Log.i("ObjectUserName",dbEmail);
-			  Log.i("ObjectPW", dbPW);
-			}
-			Log.i("Login Activity: ", "Login Clicked4");
+				  user.setUsername(usernameStr);
+				  user.setPassword(passwordStr);
+
+				  user.signUpInBackground(new SignUpCallback() {
+					  @Override
+					  public void done(ParseException e) {
+						  if (e == null){
+							  Log.i("SignUp", "Successful");
+							  Toast.makeText(getApplicationContext(),
+									  "Redirecting...", Toast.LENGTH_SHORT).show();
+							  switchToMenu();
+						  } else {
+							  Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+						  }
+					  }
+				  });
+			  }
+
 		  }
-		});
+	  });
 
-		switchToMenu();
+	  login_button.setOnClickListener(new View.OnClickListener() {
+		  @Override
+		  public void onClick(View view) {
+			  String usernameStr = username.getText().toString();
+			  String passwordStr = password.getText().toString();
 
-		 //else if email doesn't exist report it
+			  if(usernameStr.matches("") || passwordStr.matches("")) {
+				  Toast.makeText(LoginActivity.this, "A username and password are required", Toast.LENGTH_SHORT).show();
+			  }else{
 
-		 //else if email exists and password doesn't report incorrect password
-	  }
-
-
-	});
-
+				  ParseUser.logInInBackground(usernameStr, passwordStr, new LogInCallback() {
+					  @Override
+					  public void done(ParseUser user, ParseException e) {
+						  if (user != null) {
+							  Log.i("LogIn", "Successful");
+							  Log.i("LogIn", "username: " + user.getUsername());
+							  Log.i("LogIn", "password: " + user.get("password"));
+							  Toast.makeText(getApplicationContext(),
+									  "Redirecting...", Toast.LENGTH_SHORT).show();
+							  switchToMenu();
+						  } else {
+							  Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+						  }
+					  }
+				  });
+			  }
+		  }
+	  });
 
 	ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
