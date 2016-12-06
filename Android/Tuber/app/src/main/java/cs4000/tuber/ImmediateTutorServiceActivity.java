5b91b923817518f926f1a94f8ebcd510660438bd
Ali <aymomeni@gmail.com>
2016-12-06 00:13:13 -0700

    From the Tutor's view student requests can be seen in list format sorted, working now on making the connection between student and tutor

--- Android/Tuber/app/src/main/java/cs4000/tuber/ImmediateTutorServiceActivity.java
+++ Android/Tuber/app/src/main/java/cs4000/tuber/ImmediateTutorServiceActivity.java
@@ -1,14 +1,172 @@
 package cs4000.tuber;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImmediateTutorServiceActivity extends Activity {
+
+
+  //ListView
+  ListView requestListView;
+  ArrayList<String> requests = new ArrayList<String>();
+  ArrayAdapter arrayAdapter;
+  LocationManager locationManager;
+  LocationListener locationListener;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_immediate_tutor_service);
+
+	setTitle("Nearby Student Requests");
+
+	requestListView = (ListView) findViewById(R.id.requestListView);
+
+	arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, requests);
+	requests.clear();
+	requests.add("Getting nearby requests...");
+
+	requestListView.setAdapter(arrayAdapter);
+
+	locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+	locationListener = new LocationListener() {
+	  @Override
+	  public void onLocationChanged(Location location) {
+		updateListView(location);
+	  }
+
+	  @Override
+	  public void onStatusChanged(String provider, int status, Bundle extras) {
+
+	  }
+
+	  @Override
+	  public void onProviderEnabled(String provider) {
+
+	  }
+
+	  @Override
+	  public void onProviderDisabled(String provider) {
+
+	  }
+	};
+
+	// Checking for GPS permissions
+	if(Build.VERSION.SDK_INT<23) {
+
+	  locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+	} else {
+
+	  if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+		ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+
+	  } else {
+
+		// else we have permission
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+		// gets last knwon location else it'll update the location based on the current location
+		Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+		if(lastKnownLocation != null) {
+
+		  updateListView(lastKnownLocation);
+		}
+	  }
+	}
   }
+
+
+  	// Updates the list view that the tutor can view
+	public void updateListView(Location location){
+
+	  requests.clear();
+
+	  if(location != null){
+
+		ParseQuery<ParseObject>  query = ParseQuery.getQuery("Request");
+
+		final ParseGeoPoint geoPointLocation = new ParseGeoPoint(location.getLatitude(), location.getLongitude());
+		query.whereNear("location", geoPointLocation);
+		query.setLimit(10); // usually driver selects one out of the 10 closest locations
+
+		query.findInBackground(new FindCallback<ParseObject>() {
+		  @Override
+		  public void done(List<ParseObject> objects, ParseException e) {
+			if(e == null){
+
+			  if(objects.size() > 0){
+
+				for(ParseObject object : objects){
+
+				  Double distanceInMiles = geoPointLocation.distanceInMilesTo((ParseGeoPoint)object.get("location"));
+				  Double distanceOneDP = (double) Math.round(distanceInMiles * 10) /10;
+
+				  requests.add(object.get("username").toString() + " - Distance: " + distanceOneDP.toString() + " miles");
+				}
+				arrayAdapter.notifyDataSetChanged();
+			  } else {
+
+				requests.add("No active requests nearby");
+			  }
+			}
+		  }
+		});
+
+	  }
+	}
+
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+	  super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+	  if(requestCode == 1) {
+
+		if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+		  if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+			Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+			updateListView(lastKnownLocation);
+
+		  }
+		}
+	  }
+	}
+
+
+
+
+
+
 
 }
--- Android/Tuber/app/src/main/java/cs4000/tuber/StudentImmediateRequestActivity.java
+++ Android/Tuber/app/src/main/java/cs4000/tuber/StudentImmediateRequestActivity.java
@@ -3,6 +3,7 @@ package cs4000.tuber;
 import android.*;
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -14,6 +15,12 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 
 import android.location.LocationListener;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.appdatasearch.GetRecentContextCall;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,6 +28,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
 
 public class StudentImmediateRequestActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -28,6 +44,20 @@ public class StudentImmediateRequestActivity extends FragmentActivity implements
   LocationManager locationManager;
   LocationListener locationListener;
   String userType = "student";
+  Button RequestButton;
+  Boolean requestActive = false;
+
+
+  public void logout(View view) {
+
+	Log.i("Info", "Logout Student Immediate Service");
+	ParseUser.logOut();
+
+	Intent intent = new Intent(getApplicationContext(), MenuActivity2.class);
+	startActivity(intent);
+
+  }
+
 
   @Override
   public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -50,6 +80,76 @@ public class StudentImmediateRequestActivity extends FragmentActivity implements
 	}
   }
 
+
+  public void requestTutor(View view) {
+
+	Log.i("Info", "Tutor Requested");
+
+	// checking if a request is active
+	if (requestActive) {
+
+	  ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Request");
+	  query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+
+	  query.findInBackground(new FindCallback<ParseObject>() {
+		@Override
+		public void done(List<ParseObject> objects, ParseException e) {
+
+		  if(e == null){
+
+			if(objects.size() > 0){
+
+			  // if any active request exists delete them from the database
+			  for(ParseObject object: objects) {
+				object.deleteInBackground();
+			  }
+
+			  requestActive = false;
+			  RequestButton.setText("Request Tutor");
+
+			}
+		  }
+		}
+	  });
+
+	} else {
+
+	  if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+		Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+		if (lastKnownLocation != null) {
+
+		  ParseObject request = new ParseObject("Request");
+		  request.put("username", ParseUser.getCurrentUser().getUsername());
+		  ParseGeoPoint parseGeoPoint = new ParseGeoPoint(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+		  request.put("location", parseGeoPoint);
+
+		  request.saveInBackground(new SaveCallback() {
+			@Override
+			public void done(ParseException e) {
+
+			  if (e == null) {
+
+				RequestButton.setText("Cancel Request");
+				requestActive = true;
+				Log.i("Info", "Tutor Request Cancelled");
+			  }
+			}
+		  });
+
+		} else {
+
+		  Toast.makeText(this, "Could not find your location. Please try again later.", Toast.LENGTH_SHORT).show();
+
+		}
+	  }
+
+	}
+  }
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
@@ -58,6 +158,35 @@ public class StudentImmediateRequestActivity extends FragmentActivity implements
 	SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
 			.findFragmentById(R.id.map);
 	mapFragment.getMapAsync(this);
+
+	ParseUser.getCurrentUser().put("studentOrTutor", userType);
+	ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+	  @Override
+	  public void done(ParseException e) {
+		// TODO: empty for now
+	  }
+	});
+
+	RequestButton = (Button) findViewById(R.id.requestTutorButton);
+
+	ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Request");
+	query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+
+	query.findInBackground(new FindCallback<ParseObject>() {
+	  @Override
+	  public void done(List<ParseObject> objects, ParseException e) {
+
+		if(e == null){
+		  if(objects.size() > 0){
+
+			requestActive = true;
+			RequestButton.setText("Cancel Request");
+
+		  }
+		}
+	  }
+	});
+
   }
 
 
@@ -125,7 +254,7 @@ public class StudentImmediateRequestActivity extends FragmentActivity implements
 	}
   }
 
-  // Updates the map
+  // Updates the map when location of user changes
   public void updateMap(Location location) {
 
 	LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
