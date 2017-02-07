a0ba8be6589f0f3f473e9cbd523357937163e7bd
aoki19 <Anne@brandon-tobins-macbook-pro.local>
2017-02-06 23:30:33 -0700

    can leave study hotspot, need to work on deleting study hotspot.

--- iOS/Tuber/Tuber/ActiveHotspotViewController.swift
+++ iOS/Tuber/Tuber/ActiveHotspotViewController.swift
@@ -20,8 +20,66 @@ class ActiveHotspotViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+
+    @IBAction func deleteLeaveButton(_ sender: Any) {
+        let server = "http://tuber-test.cloudapp.net/ProductRESTService.svc/leavestudyhotspot"
+        
+        //created NSURL
+        let requestURL = NSURL(string: server)
+        
+        //creating NSMutableURLRequest
+        let request = NSMutableURLRequest(url: requestURL! as URL)
+        
+        //setting the method to post
+        request.httpMethod = "POST"
+        
+        let defaults = UserDefaults.standard
+        
+        //getting values from text fields
+        let userEmail = defaults.object(forKey: "userEmail") as! String
+        let userToken = defaults.object(forKey: "userToken") as! String
+//        let course = defaults.object(forKey: "selectedCourse") as! String
+        //        let latitude = String(describing: location?.coordinate.latitude)
+        //        let longitude = String(describing: location?.coordinate.longitude)
+        
+        //creating the post parameter by concatenating the keys and values from text field
+        let postParameters = "{\"userEmail\":\"\(userEmail)\",\"userToken\":\"\(userToken)\"}"
+        
+        print(postParameters)
+        
+        //adding the parameters to request body
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        print(postParameters)
+        
+        //creating a task to send the post request
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            data, response, error in
+            
+            if error != nil{
+                print("error is \(error)")
+                return;
+            }
+            
+            let r = response as? HTTPURLResponse
+            
+            if (r?.statusCode == 200)
+            {
+                OperationQueue.main.addOperation{
+                    self.performSegue(withIdentifier: "reloadMap", sender: nil)
+                }
+            }
+            else{
+                print(r?.statusCode as Any)
+            }
+            
+        }
+        //executing the task
+        task.resume()
+    }
     /*
     // MARK: - Navigation
 
