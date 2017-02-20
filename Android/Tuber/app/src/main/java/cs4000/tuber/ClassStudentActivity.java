f1027ef9e7c2ce4401c6ed59fad8ddd436eec1cd
Ali <aymomeni@gmail.com>
2017-02-20 03:29:33 -0700

    I made a miracle happen. I fixed a professional's library by copying the source code and fixing the error in his code.

--- Android/Tuber/app/src/main/java/cs4000/tuber/ClassStudentActivity.java
+++ Android/Tuber/app/src/main/java/cs4000/tuber/ClassStudentActivity.java
@@ -1,17 +1,17 @@
 package cs4000.tuber;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-
 /*
  * After accessing an academic class, this view displays
  * Options that the user can select in the context of the
  * given academic class
  */
-public class ClassStudentActivity extends Activity {
+public class ClassStudentActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +20,17 @@ public class ClassStudentActivity extends Activity {
     }
 
     public void class_option_tutorServices_clicked(View view) {
-	    Log.i("@TutorServices", getIntent().getStringExtra("course"));
+	    Log.i("TutorService", getIntent().getStringExtra("course"));
         Intent intent = new Intent(ClassStudentActivity.this, TutorServicesActivity.class);
         intent.putExtra("course", getIntent().getStringExtra("course"));
         startActivity(intent);
+    }
+
+    public void class_option_study_hotspot_clicked(View view) {
+        Log.i("StudyHotspot", getIntent().getStringExtra("course"));
+        Intent intent = new Intent(ClassStudentActivity.this, HotspotActivity.class);
+        intent.putExtra("course", getIntent().getStringExtra("course"));
+        startActivity(intent);
+
     }
 }
---
+++ Android/Tuber/app/src/main/java/cs4000/tuber/HotspotActivity.java
@@ -0,0 +1,47 @@
+package cs4000.tuber;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+/**
+ * Created by Ali on 2/20/2017.
+ */
+public class HotspotActivity extends AppCompatActivity implements MapViewPager.Callback {
+
+    private ViewPager viewPager;
+    private MapViewPager mvp;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotspot);
+
+        SupportMapFragment map = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setPageMargin(HotspotUtils.dp(this, 18));
+        HotspotUtils.setMargins(viewPager, 0, 0, 0, HotspotUtils.getNavigationBarHeight(this));
+
+        mvp = new MapViewPager.Builder(this)
+                .mapFragment(map)
+                .viewPager(viewPager)
+                .position(2)
+                .adapter(new HotspotAdapter(getSupportFragmentManager()))
+                .callback(this)
+                .build();
+    }
+
+    @Override
+    public void onMapViewPagerReady() {
+        mvp.getMap().setPadding(
+                0,
+                HotspotUtils.dp(this, 40),
+                HotspotUtils.getNavigationBarWidth(this),
+                viewPager.getHeight() + HotspotUtils.getNavigationBarHeight(this));
+    }
+
+}
