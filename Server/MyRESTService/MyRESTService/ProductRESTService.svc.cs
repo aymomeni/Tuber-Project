47dca2421f4a833b3a78286dd7852c42191e1a39
tobin53 <brandontobin@cox.net>
2017-03-21 10:10:18 -0700

    Cleaned up the RemoveStudentClasses method.

--- Server/MyRESTService/MyRESTService/ProductRESTService.svc.cs
+++ Server/MyRESTService/MyRESTService/ProductRESTService.svc.cs
@@ -793,7 +793,6 @@ namespace ToDoList
                     {
                         conn.Open();
                         transaction = conn.BeginTransaction();
-
                         MySqlCommand command = conn.CreateCommand();
                         command.Transaction = transaction;
 
