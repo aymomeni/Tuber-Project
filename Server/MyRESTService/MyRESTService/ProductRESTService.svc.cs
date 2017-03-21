4a4e898fb2007a7ec538d9b1425c176a0ec28907
tobin53 <brandontobin@cox.net>
2017-03-21 10:06:06 -0700

    Cleaned up EnableTutoring method

--- Server/MyRESTService/MyRESTService/ProductRESTService.svc.cs
+++ Server/MyRESTService/MyRESTService/ProductRESTService.svc.cs
@@ -379,7 +379,6 @@ namespace ToDoList
                         {
                             conn.Open();
                             transaction = conn.BeginTransaction();
-
                             MySqlCommand command = conn.CreateCommand();
                             command.Transaction = transaction;
 
