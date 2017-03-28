1b623c943be4f66901958268fa2b9625fd86aff2
aoki19 <Anne@brandon-tobins-macbook-pro.local>
2017-03-28 01:52:45 -0600

    report tutor fixed

--- iOS/Tuber/Tuber/ReportTutorListTableViewCell.swift
+++ iOS/Tuber/Tuber/ReportTutorListTableViewCell.swift
@@ -14,6 +14,7 @@ class ReportTutorListTableViewCell: UITableViewCell {
     @IBOutlet weak var tutorNameLabel: UILabel!
     
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datetimeLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
---
+++ iOS/Tuber/Tuber/ReportTutorSessionsTableViewController.swift
@@ -0,0 +1,132 @@
+//
+//  ReportTutorSessionsTableViewController.swift
+//  Tuber
+//
+//  Created by Anne on 3/28/17.
+//  Copyright © 2017 Tuber. All rights reserved.
+//
+
+import UIKit
+
+class ReportTutorSessionsTableViewController: UITableViewController {
+
+    var postParameters: [String] = []
+    var sessionStartTime: [String] = []
+    var sessionID: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return sessionStartTime.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tutorSessions", for: indexPath) as! ReportTutorListTableViewCell
+        
+        cell.datetimeLabel.text = sessionStartTime[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let indexPath = tableView.indexPathForSelectedRow //optional, to get from any UIButton for example
+        let session = sessionID[(indexPath?.row)!]
+        
+        var params = postParameters[0]
+        params += "\",\"tutorSessionID\":\"\(session)\",\"message\":"
+        
+        self.performSegue(withIdentifier: "loadReportForm", sender: params)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "loadReportForm"
+        {
+            let parameter = sender as! String
+            print(parameter)
+            
+            if let destination = segue.destination as? ReportTutorFormViewController
+            {
+                destination.tempPostParameters = ""
+                
+                destination.tempPostParameters = parameter
+            }
+        }
+    }
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
