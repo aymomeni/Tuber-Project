f60ee1d33c2fe291d385ce5b35c38f0e293f0d18
Ali <aymomeni@gmail.com>
2017-04-09 21:40:27 -0600

    Added fragment text that represents hotspot student's info

--- Android/Tuber/app/src/main/java/cs4000/tuber/HotspotFragment.java
+++ Android/Tuber/app/src/main/java/cs4000/tuber/HotspotFragment.java
@@ -14,13 +14,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HotspotFragment extends Fragment implements View.OnClickListener {
 
     private Toolbar toolbar;
     private int index;
     private Button mJoinButton;
+    private ArrayAdapter<String> mListAdapter;
+    private Adapter mStringAdapter;
+    private ListView mHotspotInformationListView;
+    private View mFragmentView;
 
     public HotspotFragment() { }
 
@@ -37,11 +46,11 @@ public class HotspotFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.hotspot_fragment, container, false);
-        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
-        mJoinButton = (Button) view.findViewById(R.id.hotspot_pager_join_button);
+        mFragmentView = inflater.inflate(R.layout.hotspot_fragment, container, false);
+        toolbar = (Toolbar) mFragmentView.findViewById(R.id.toolbar);
+        mJoinButton = (Button) mFragmentView.findViewById(R.id.hotspot_pager_join_button);
         mJoinButton.setOnClickListener(this);
-        return view;
+        return mFragmentView;
     }
 
     @Override
@@ -61,6 +70,25 @@ public class HotspotFragment extends Fragment implements View.OnClickListener {
                 getActivity().onBackPressed();
             }
         });
+
+
+        ArrayList<String> listElements = new ArrayList<String>();
+
+        if(HotspotAdapter.getmDataSet().size() > 0){
+
+            //listElements.add("User: " + HotspotAdapter.getmDataSet().get(index).getmOwnerEmail() + "\n" + "Student Count: " + HotspotAdapter.getmDataSet().get(index).getmStudentCount() + "\n");
+            listElements.add("User: <Name> " + "\n" + "Distance: " + String.format("%.1f", HotspotAdapter.getmDataSet().get(index).getMdistanceToHotspot()) + " miles" + "\n" +"Student Count: " + HotspotAdapter.getmDataSet().get(index).getmStudentCount() + "\n");
+            mListAdapter = new ArrayAdapter<String>(getContext(), R.layout.hotspot_fragment_text_elements, listElements);
+
+            mHotspotInformationListView = (ListView) this.mFragmentView.findViewById(R.id.hotspot_fragment_text_list_view);
+
+            mHotspotInformationListView.setAdapter(mListAdapter);
+        }
+
+
+
+
+
     }
 
     @Override
@@ -72,6 +100,7 @@ public class HotspotFragment extends Fragment implements View.OnClickListener {
                 ViewPager vp = (ViewPager) (v.getParent().getParent().getParent().getParent());
 
                 Log.i("HotspotFragOnClick", "Clicked " + vp.getCurrentItem());
+                Log.i("HSDataElement", HotspotAdapter.getmDataSet().get(vp.getCurrentItem()).getmOwnerEmail());
                 break;
 //            case R.id.textView_settings:
 //                switchFragment(SettingsFragment.TAG);
