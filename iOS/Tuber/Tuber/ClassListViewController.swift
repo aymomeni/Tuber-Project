76684000acd2c818c4b4d31e676aedd7c6f74ed0
aoki19 <Anne@brandon-tobins-macbook-pro.local>
2017-04-22 04:26:29 -0600

    reverted slightly for development purposes

--- iOS/Tuber/Tuber/ClassListViewController.swift
+++ iOS/Tuber/Tuber/ClassListViewController.swift
@@ -76,7 +76,8 @@ class ClassListViewController: UIViewController, UITableViewDataSource, UITableV
         let indexPath = tableView.indexPathForSelectedRow //optional, to get from any UIButton for example
         let currentCell = tableView.cellForRow(at: indexPath!)! as! ClassTableViewCell
         UserDefaults.standard.set(currentCell.classNameLabel.text! as String?, forKey: "selectedCourse")
-        performSegue(withIdentifier: "studentOptions", sender: nil)
+//        performSegue(withIdentifier: "studentOptions", sender: nil)
+        performSegue(withIdentifier: "shortcut", sender: nil)
     }
     
     // MARK: - ButtonCellDelegate
